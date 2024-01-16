ed0e71de0ae30fd5cddd5cd4577b76246fb0293f
Ibrahim8879 <ibrahimqayyum7891234@gmail.com>
2024-01-16 18:29:23 +0500

    Part-1
    I added earth now next is to add styles etc.

---
+++ components/model.js
@@ -0,0 +1,32 @@
+"use client";
+
+import { Suspense } from "react"
+import { Canvas, useLoader } from "@react-three/fiber"
+import { Environment, OrbitControls } from "@react-three/drei"
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
+
+const Model = () => {
+  // location of the 3D model
+  const gltf = useLoader(GLTFLoader, "/earth/scene.gltf");
+  return (
+    <group>
+      <primitive object={gltf.scene} scale={1.3} />
+    </group>
+  );
+};
+
+export default function Model_page() {
+  return (
+    <div className="earth">
+        <Canvas shadows dpr={[1, 2]} camera={{ position: [10, 3, 5], fov: 70 }}>
+          <ambientLight intensity={1} />
+          <spotLight intensity={0.5} angle={0.1} penumbra={1} position={[10, 15, 10]} castShadow />
+          <Suspense fallback={null}>
+            <Model />
+            <Environment preset="city" />
+          </Suspense>
+          <OrbitControls autoRotate />
+        </Canvas>
+      </div>
+  )
+}

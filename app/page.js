5a44db70148c3fca0f3bfd7aed9251de74a93933
Ibrahim8879 <ibrahimqayyum7891234@gmail.com>
2024-02-13 01:00:50 +0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,13 +1,13 @@
 import Model_page from "@/components/Model_s/model"
 import Card_Section from "@/components/Card/Main_card_page"
 import BarLineGraph from "@/components/Graph/BarGraph"
-//<Model_page/>
-//<Card_Section/>
+
 export default function Home() {
 
   return (
     <div>
-
+      <Model_page/>
+      <Card_Section/>
       <BarLineGraph/>
     </div>
   )
